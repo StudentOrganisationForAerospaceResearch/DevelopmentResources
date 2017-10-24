da22491e7b649bf38bb4eeac3038e7e7ad922803
Enoch Tsang <etsang1@hotmail.com>
2017-10-23 18:15:25 -0600

    Improve instructions in basic training

--- Training/BasicCppTraining/Person.h
+++ Training/BasicCppTraining/Person.h
@@ -1,29 +1,29 @@
-// This line is a preprocessor directive that this header is only defined once.
-// This needs to be included in each header file.
 #pragma once
 
 #include <string>
+
 class Person
 {
 public:
-// A Rule of Thumb when designing classes is to have a default constructor, regular constructor, and a destructor.
     Person();
-    Person(const std::string& name, int age);
     ~Person();
+    Person(const std::string& name, int age);
 
+    // setters and getters
     int getAge() const;
     const std::string& getName() const;
-
-    void printInfo();//Print out the name and age of the person.
-
     void setName(const std::string& newName);
     void setAge(int newAge);
 
-    static int combinedAge(Person** x, int size); // Print the combined Age of everyone pointed to by x
-    static void birthday(Person& x); // Increases the Person's Age by 1.
+    // Print out the name and age of the person.
+    void printInfo() const;
+    // Print the combined age of everyone pointed to by personArray
+    static int combinedAge(Person** personArray, int size);
+    // Increases the Person's Age by 1.
+    static void birthday(Person& x);
+
 private:
     std::string name_;
-
     int* age_; // age_ should be a dynamically allocated.
 
 };
--- Training/BasicCppTraining/main.cpp
+++ Training/BasicCppTraining/main.cpp
@@ -1,6 +1,7 @@
-#include <iostream> //Imports cout
+#include <iostream>
 #include <sstream>
 #include <string>
+
 #include "Person.h"
 
 void compareAnswers(const std::string myAnswer, const std::string correctAnswer)
@@ -26,45 +27,57 @@ void myIncrement(int* x)
 }
 
 // Uncomment this when person.cpp has been created.
-/* void personFunctions()
+void personFunctions()
 {
     Person Bill;
     Bill.setName("Bill");
     Bill.setAge(18);
     Bill.printInfo();
+
     Person Bob("Bob", 30);
     Bob.printInfo();
+
     Person* Joe = new Person("Joe", 15);
     Joe->printInfo();
+
     Person* personArray[] = {&Bill, &Bob, Joe};
     std::stringstream myCombinedAgeAnswer;
     myCombinedAgeAnswer << "The combined age of Bill, Bob, and Joe is: " << Person::combinedAge(personArray, sizeof(personArray) / sizeof(Person*));
+
     const std::string correctCombinedAgeAnswer = "The combined age of Bill, Bob, and Joe is: 63";
     std::cout << "Checking Person::combinedAge:" << std::endl
               << myCombinedAgeAnswer.str() << std::endl;
     compareAnswers(myCombinedAgeAnswer.str(), correctCombinedAgeAnswer);
+
     Person::birthday(Bill);
     std::stringstream myBirthdayAnswer;
     myBirthdayAnswer << "It was Bill's Birthday, he is now " << Bill.getAge();
+
     const std::string correctBirthdayAnswer = "It was Bill's Birthday, he is now 19";
     std::cout << "Checking Person::Birthday:" << std::endl
               << myBirthdayAnswer.str() << std::endl;
     compareAnswers(myBirthdayAnswer.str(), correctBirthdayAnswer);
     delete Joe;
-}*/
+}
 
 int main()
 {
     int a = 5;
     int b = 6;
-    std::stringstream swapIncrementAnswer;
+
+// Pass a & b into mySwap here
+// Pass in a to myIncrement here
+
     std::cout << "Checking Swap and Increment: " << std::endl;
+
+    std::stringstream swapIncrementAnswer;
     swapIncrementAnswer << "a is " << a << ", b is " << b;
     std::cout << swapIncrementAnswer.str() << std::endl;
     const std::string correctSwapIncrementAnswer = "a is 7, b is 5";
+
     compareAnswers(swapIncrementAnswer.str(), correctSwapIncrementAnswer);
-// Pass a & b into mySwap
-// Pass in a to myIncrement
+
     //personFunctions(); // Uncomment this once you have completed the definitions of the Person class.
+
     return 0;
 }
