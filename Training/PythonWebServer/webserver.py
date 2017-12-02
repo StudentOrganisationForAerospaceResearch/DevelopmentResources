e41e357c83d9b897b539755315e59ff2061d2e4b
ljhaigh <ljhaigh@gmail.com>
2017-12-02 09:41:54 -0700

    Using BaseHTTPRequestHandler; POST and GET work; TODO serve css and js

--- Training/PythonWebServer/webserver.py
+++ Training/PythonWebServer/webserver.py
@@ -1,14 +1,33 @@
 #!/usr/bin/env python
 
-from http.server import SimpleHTTPRequestHandler, HTTPServer
-import sys
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import sys, psutil, time
 
-class SoarWebServer(SimpleHTTPRequestHandler):
-
+class SoarWebServer(BaseHTTPRequestHandler):
+    def setHeaders(self, MIME_Type): #TODO make this thing work
+        self.send_response(200)
+        self.send_header("Content-Type", MIME_Type)
+        self.end_headers()
+        
     def do_POST(self):
-        self.wfile.write("Send your POST response here!")
+        self.send_response(200)
+        self.send_header("Content-Type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("\nSend your POST response here!", "utf-8"))
+        
+    def do_GET(self):
+        #setHeaders(self, "text/html")
+        self.send_response(200)
+        self.send_header("Content-Type", "text/html")
+        self.end_headers()
 
+        
+        #self.wfile.write(bytes("<!doctype html><body><p>This is a test.</p></body></html>", "utf-8"))
+        myIndex = open("index.html") #TODO wrap this in exception handling
+        self.wfile.write(bytes( myIndex.read(), "utf-8" ))
+        myIndex.close();
 
+    
 def main():
     if (len(sys.argv) == 1):
         print("Enter the port number! eg \"webserver.py 80\"")
@@ -18,6 +37,7 @@ def main():
         print("The port entered is invalid!")
         return
     
+    psutil.cpu_percent() #call this once to warm it up (returns zero first time)
     print("Starting server on port " + str(port) + "...")
     sys.stdout.flush() #I seem to need this for it to print
     server_address = ("", port)
