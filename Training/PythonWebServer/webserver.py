b31b74ca581d8c5b289c18d8de3e35989bcedd16
protocorbulo <protocorbulo@gmail.com>
2018-01-06 12:50:10 -0700

    added error checking to port number

--- Training/PythonWebServer/webserver.py
+++ Training/PythonWebServer/webserver.py
@@ -10,12 +10,33 @@ class SoarWebServer(SimpleHTTPRequestHandler):
 
 
 def main():
-    port = int(sys.argv[1]) # Error check this!
+
+    if len(sys.argv)<2:
+        print("No Port Specified")
+        doNotStart()
+
+    try:
+        port = int(sys.argv[1])
+    except:
+        print("Cannot create server on port " + str(sys.argv[1]))
+        doNotStart()
+
+    if (port<1024) or (port>49152):
+        print("Please specify a port between 1024 and 49152 (8000 recommended)")
+        doNotStart()
+
     print("Starting server on port " + str(port) + "...")
 
     server_address = ("", port)
     httpd = HTTPServer(server_address, SoarWebServer)
     httpd.serve_forever()
+
+
+def doNotStart():
+    print("Webserver will not start")
+    exit()
+
+
 
 if __name__ == "__main__":
     main()
