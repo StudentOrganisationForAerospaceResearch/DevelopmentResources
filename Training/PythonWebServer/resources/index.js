ed198c951309fad027757bdc47bebdc0fe51be77
NataliaP721 <natalia.pavlovic@ucalgary.ca>
2018-01-15 21:21:52 -0700

    Made changes

--- Training/PythonWebServer/resources/index.js
+++ Training/PythonWebServer/resources/index.js
@@ -1,3 +1,8 @@
 $('document').ready(function(){
     // Put your ajax request here
+    $(document).ready(function(){
+    $("button").click(function(){
+    	$("p").show();
+    });
+});
 });
--- Training/PythonWebServer/webserver.py
+++ Training/PythonWebServer/webserver.py
@@ -2,16 +2,42 @@
 
 from http.server import SimpleHTTPRequestHandler, HTTPServer
 import sys
+import psutil
+from datetime import datetime
+import pytz
 
 class SoarWebServer(SimpleHTTPRequestHandler):
 
     def do_POST(self):
-        self.wfile.write("Send your POST response here!")
+        #Send your POST response here!
+        s0 = str(datetime.now(tz=pytz.timezone('America/Edmonton')).strftime('%H:%M:%S'));
+        s1 = str(psutil.cpu_count());
+        s2 = str(psutil.cpu_percent(interval=1));
+       	s3 = str(round(psutil.virtual_memory().total/(2**30), 2));
+        s4 = str(round(psutil.virtual_memory().used/(2**30), 2));
+
+        self.wfile.write(bytes(s0, "utf-8"));
+        self.wfile.write(bytes(s1, "utf-8"));
+        self.wfile.write(bytes(s2, "utf-8"));
+        if(len(s2)==3):
+        	self.wfile.write(bytes(" ", "utf-8"));
+        if(len(s2)==2):
+        	self.wfile.write(bytes("  ", "utf-8"));
+        self.wfile.write(bytes(s3, "utf-8"));
+        if(len(s4)==3):
+        	self.wfile.write(bytes("  ", "utf-8"));
+        if(len(s3)==4):
+        	self.wfile.write(bytes(" ", "utf-8"));
+        self.wfile.write(bytes(s4, "utf-8"));
+        if(len(s4)==3):
+        	self.wfile.write(bytes("  ", "utf-8"));
+        if(len(s4)==4):
+        	self.wfile.write(bytes(" ", "utf-8"));
 
 
 def main():
     port = int(sys.argv[1]) # Error check this!
-    print("Starting server on port " + str(port) + "...")
+    print(datetime.now(tz=pytz.timezone('America/Edmonton')).strftime('%H:%M:%S'))
 
     server_address = ("", port)
     httpd = HTTPServer(server_address, SoarWebServer)
