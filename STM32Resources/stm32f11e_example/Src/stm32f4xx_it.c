66be4fbb134984375b84c429f454fc4432166aa1
NataliaP721 <natalia.pavlovic@ucalgary.ca>
2019-11-23 11:51:20 -0700

    Changed TIM to tim in makefile

--- STM32Resources/stm32f11e_example/Src/stm32f4xx_it.c
+++ STM32Resources/stm32f11e_example/Src/stm32f4xx_it.c
@@ -65,7 +65,7 @@ extern TIM_HandleTypeDef htim1;
 /* USER CODE END EV */
 
 /******************************************************************************/
-/*           Cortex-M4 Processor Interruption and Exception Handlers          */
+/*           Cortex-M4 Processor Interruption and Exception Handlers          */ 
 /******************************************************************************/
 
 /******************************************************************************/
@@ -80,13 +80,13 @@ extern TIM_HandleTypeDef htim1;
   */
 void TIM1_UP_TIM10_IRQHandler(void)
 {
-    /* USER CODE BEGIN TIM1_UP_TIM10_IRQn 0 */
+  /* USER CODE BEGIN TIM1_UP_TIM10_IRQn 0 */
 
-    /* USER CODE END TIM1_UP_TIM10_IRQn 0 */
-    HAL_TIM_IRQHandler(&htim1);
-    /* USER CODE BEGIN TIM1_UP_TIM10_IRQn 1 */
+  /* USER CODE END TIM1_UP_TIM10_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim1);
+  /* USER CODE BEGIN TIM1_UP_TIM10_IRQn 1 */
 
-    /* USER CODE END TIM1_UP_TIM10_IRQn 1 */
+  /* USER CODE END TIM1_UP_TIM10_IRQn 1 */
 }
 
 /* USER CODE BEGIN 1 */
